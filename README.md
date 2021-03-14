31d40ab9696809e0a23189be79e34928c1a8c422
dylanfaires <dylanfaires@gmail.com>
2021-03-14 18:29:09 +0000

    Added data and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Reporting Scripts
+
+**All Data is fake data of a typical 3rd Party Technology Investment Portfolio for a F500.**
---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("Data/providerportfolio.csv")
+
+print(data.head())
+
